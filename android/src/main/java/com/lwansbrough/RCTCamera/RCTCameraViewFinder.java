27948bd765c4423971f30a09478bbede431175c4
Kevin Nguy <kevnguy@gmail.com>
2017-04-21 17:52:31 -0700

    Fix boolean syntax error

--- android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraViewFinder.java
+++ android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraViewFinder.java
@@ -462,7 +462,7 @@ class RCTCameraViewFinder extends TextureView implements TextureView.SurfaceText
             try {
                 _camera.autoFocus(new Camera.AutoFocusCallback() {
                     @Override
-                    public void onAutoFocus(boolean b, Camera camera) {
+                    public void onAutoFocus(boolean success, Camera camera) {
                         if (success) {
                             camera.cancelAutoFocus();
                         }                    
